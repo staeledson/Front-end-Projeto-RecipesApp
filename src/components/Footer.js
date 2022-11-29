7815c741bb0bfbe3523b2e3fb7791c7279cbf244
Leila Moraes <leila.violino@gmail.com>
2022-11-29 20:53:21 -0300

    Exclui o componente Button e troca os buttons do Footer para Links. Altera o css do footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Button from './Button';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 import './Footer.css';
@@ -7,30 +8,28 @@ import './Footer.css';
 export default function Footer() {
   return (
     <div className="footer" data-testid="footer">
-      <Button
-        btnClass="drinks"
-        id="drinks"
-        btnName={
-          <img
-            data-testid="drinks-bottom-btn"
-            src={ drinkIcon }
-            alt="taça de drink"
-            id="drinks"
-          />
-        }
-      />
-      <Button
-        btnClass="meals"
-        id="meals"
-        btnName={
-          <img
-            data-testid="meals-bottom-btn"
-            src={ mealIcon }
-            alt="garfo e colher cruzados em um X"
-            id="meals"
-          />
-        }
-      />
+      <Link to="/drinks" className="link-drinks">
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="taça de drink"
+          id="drinks"
+        />
+      </Link>
+      <Link to="/meals" className="link-meals">
+        <img
+          data-testid="meals-bottom-btn"
+          src={ mealIcon }
+          alt="garfo e colher cruzados em um X"
+          id="meals"
+        />
+      </Link>
     </div>
   );
 }
+
+Footer.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
