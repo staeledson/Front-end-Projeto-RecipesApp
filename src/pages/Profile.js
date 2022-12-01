bb4ba0491846b313033104babd3ae9b59f282397
tulio <tulio.amorimm@gmail.com>
2022-12-01 16:53:40 -0300

    add req-57-62

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,51 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 
 function Profile() {
+  const history = useHistory();
+  // const userSaved = JSON.parse(localStorage.getItem('user'));
+
+  const onClickToDoneRecipes = () => {
+    history.push('/done-recipes');
+  };
+
+  const onClickToFavoriteRecipes = () => {
+    history.push('/favorite-recipes');
+  };
+
+  const onClickToLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
-    <Header />
+    <div>
+      {/* <h2>{ userSaved }</h2> */}
+      <Header />
+      <h2 data-testid="profile-email">email</h2>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ (() => onClickToDoneRecipes()) }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ (() => onClickToFavoriteRecipes()) }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ (() => onClickToLogout()) }
+      >
+        Logout
+      </button>
+    </div>
   );
 }
 
---
+++ src/tests/Profile.test.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWith';
+import Profile from '../pages/Profile';
+import App from '../App';
+
+describe('esta a página de Profile', () => {
+  test('se é renderizado 3 botoes na tela', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/profile');
+    });
+
+    const doneRecipesButton = screen.getByRole('button', { name: /done recipes/i });
+    expect(doneRecipesButton).toBeInTheDocument();
+
+    const favoriteRecipesButton = screen.getByRole('button', { name: /favorite recipes/i });
+    expect(favoriteRecipesButton).toBeInTheDocument();
+
+    const logoutButton = screen.getByRole('button', { name: /logout/i });
+    expect(logoutButton).toBeInTheDocument();
+  });
+
+  test('se tem um "h1" na tela com nome "Profile"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/profile');
+    });
+
+    const profileTitle = screen.getByRole('heading', { name: /profile/i });
+    expect(profileTitle).toBeInTheDocument();
+  });
+
+  test('se ao clicar no botao "Done Recipes" vai para a rota "/done-recipes"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/profile');
+    });
+
+    const doneRecipesButton = screen.getByRole('button', { name: /done recipes/i });
+    userEvent.click(doneRecipesButton);
+
+    expect(history.location.pathname).toBe('/done-recipes');
+  });
+
+  test('se ao clicar no botao "Favorite Recipes" vai para a rota "/done-recipes"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/profile');
+    });
+
+    const favoriteRecipesButton = screen.getByRole('button', { name: /favorite recipes/i });
+    userEvent.click(favoriteRecipesButton);
+
+    expect(history.location.pathname).toBe('/favorite-recipes');
+  });
+
+  test('se ao clicar no botao "Logout" vai para a rota "/"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/profile');
+    });
+
+    const logoutButton = screen.getByRole('button', { name: /logout/i });
+    userEvent.click(logoutButton);
+
+    expect(history.location.pathname).toBe('/');
+  });
+});
