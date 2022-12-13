0560957cbfbfc4677f07599ce7e07437a10b5eef
tulio <tulio.amorimm@gmail.com>
2022-12-13 19:36:14 -0300

    fix: solve lint issue

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './Routes';
-import ContextAppProvider from './context/ContextAppProvider';
 
 function App() {
   return (
